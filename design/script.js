7c700420183532a0dd4f1d689eff4f333a35c2b0
Felpzzcr7 <felipinholeandro0@gmail.com>
2026-02-12 15:13:38 -0300

    links add, JS criado, botoes scroll feitos

---
+++ design/script.js
@@ -0,0 +1,7 @@
+function moverDireita() {
+    document.getElementById("carrosselCompras").scrollLeft += 400;
+}
+
+function moverEsquerda() {
+    document.getElementById("carrosselCompras").scrollLeft -= 400;
+}
